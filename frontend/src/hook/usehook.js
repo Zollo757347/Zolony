35834926e5eab1567028a5a1817182251eb8e9fa
aquagamer <j00968361>
2023-01-05 00:43:26 +0800

    usehook finish

--- frontend/src/hook/usehook.js
+++ frontend/src/hook/usehook.js
@@ -8,17 +8,11 @@ const [user, setUser] = useState('');
 const [password, setPassword] = useState('');
 const [isLogIn, setIsLogIn] = useState(false);
 
-const [createAccountMutation] = useMutation(CREATE_ACCOUNT);
-const [editProfileMutation] = useMutation(EDIT_PROFILE);
-const [initialMyMapMutation] = useMutation(INITIAL_MY_MAP);
-const [editMyMapMutation] = useMutation(EDIT_MY_MAP);
-const [deleteUserMutation] = useMutation(DELETE_USER);
-const [deleteUserMapMutation] = useMutation(DELETE_USER_MAP);
 
 
 const logIn = (name, pwd) => {
     const cryptopwd = CryptoJs.MD5(pwd).toString();
-    const {loading, data} = useQuery(LOG_IN, {
+    const {loading, data, error} = useQuery(LOG_IN, {
         variables: {
             data: {
                 name: name,
@@ -27,17 +21,26 @@ const logIn = (name, pwd) => {
         }
     });
     if(loading) return 'loading...';
+    if(error){
+        console.log(`[logIn function error]: ${error.message}.`);
+        return 'error';
+    }
     else {
-        if(!data.logIn) return 'not found';
+        if(!data.logIn){
+            console.log(`user not found.`);
+            return 'user not found';
+        } 
         setUser(name);
         setPassword(cryptopwd);
         setIsLogIn(true);
+        console.log(`data query succeed.`);
+        console.log(data.logIn);
         return data.logIn;
     }
 }
 
 const getMap = (name, mapName) => {
-    const {loading, data} = useQuery(GET_MAP, {
+    const {loading, data, error} = useQuery(GET_MAP, {
         variables: {
             data: {
                 name: name,
@@ -46,15 +49,25 @@ const getMap = (name, mapName) => {
         }
     });
     if(loading) return 'loading...';
+    if(error){
+        console.log(`[getMap function error]: ${error.message}.`);
+        return 'error';
+    }
     else {
-        if(!data.getMap) return 'not found';
+        if(!data.getMap){
+            console.log(`user not found.`);
+            return 'user not found';
+        }
+        console.log(`getmap succeed.`);
+        console.log(data.getMap);
         return data.getMap;
     }
 }
 
 const createAccount = async (name, pwd) => {
     const cryptopwd = CryptoJs.MD5(pwd).toString();
-    const {data, loading} = await createAccountMutation({
+    const [createAccountMutation, {loading, data, error}] = useMutation(CREATE_ACCOUNT);
+    await createAccountMutation({
         variables: {
             data: {
                 name: name,
@@ -63,12 +76,19 @@ const createAccount = async (name, pwd) => {
         }
     }) 
     if(loading) return 'loading...';
+    if(error){
+        console.log(`[createAccount function error]: ${error.message}.`);
+        return 'error';
+    }
     else {
-        if(!data.createAccount) return '';
+        if(!data.createAccount){
+            console.log(`${name} already used.`);
+            return '';
+        } 
         setUser(name);
         setPassword(cryptopwd);
         setIsLogIn(true);
-        return data.logIn;
+        return data.createAccount;
     }
 }
 
@@ -89,7 +109,8 @@ input = {
 */
 
 const editProfile = async (input) => {
-    const {data, loading} = await editProfileMutation({
+    const [editProfileMutation, {data, loading, error}] = useMutation(EDIT_PROFILE);
+    await editProfileMutation({
         variables: {
             data: {
                 name: user,
@@ -102,27 +123,144 @@ const editProfile = async (input) => {
         }
     })
     if(loading) return 'loading...';
+    if(error){
+        console.log(`[editProfile function error]: ${error.message}.`);
+        return 'error';
+    }
     else {
-        if(!data.createAccount) return '';
+        if(!data.editProfile){
+            console.log(`user not found.`);
+            return 'user not found';
+        } 
         setPassword(CryptoJs.MD5(input.newPassword).toString());
+        console.log(`edit profile succeed.`)
         return data.editProfile;
     }
 }
 
-const initialMyMap = async () => {
-    
+const initialMyMap = async (xLen, yLen, zLen, mapName) => {
+    const [initialMyMapMutation, {data, error, loading}] = useMutation(INITIAL_MY_MAP);
+    await initialMyMapMutation({
+        variables: {
+            data: {
+                name: user,
+                password: password,
+                mapName: mapName,
+                xLen: xLen,
+                yLen: yLen,
+                zLen: zLen,
+            }
+        }
+    })
+    if(loading) return 'loading...';
+    if(error){
+        console.log(`[initialMyMap function error]: ${error.message}.`);
+        return 'error';
+    }
+    else {
+        if(!data.initialMyMap){
+            console.log(`map already exist.`);
+            return 'map already exist';
+        } 
+        console.log(`initialmap succeed`);
+        console.log(data.initialMyMap);
+        return data.initialMyMap;
+    }
 }
 
-const editMyMap = async () => {
+/* must wrap into a object
 
+map = {
+    xLen,
+    yLen,
+    zLen,
+    mapName,
+    playground [[[{
+        blockName,
+        type,
+        breakable,
+        states {
+            power,
+            source,
+            delay,
+            facing,
+            face,
+            locked,
+            powered,
+            lit,
+            east,
+            south,
+            west,
+            north,
+        }
+    }]]],
+}
+*/
+
+const editMyMap = async (map) => {
+    const [editMyMapMutation, {data, error, loading}] = useMutation(EDIT_MY_MAP);
+    await editMyMapMutation({
+        variables: {
+            data: {
+                name: user,
+                password: password,
+                mapName: map.mapName,
+                map: map,
+            }
+        }
+    })
+    if(loading) return 'loading...';
+    if(error){
+        console.log(`[editMyMap function error]: ${error.message}.`);
+        return 'error';
+    }
+    else {
+        if(!data.editMyMap){
+            console.log(`save map fail.`);
+            return 'map already exist';
+        } 
+        console.log(`edit map succeed.`)
+        console.log(data.editMyMap);
+        return data.editMyMap;
+    }
 }
 
 const deleteUser = async () => {
-
+    const [deleteUserMutation, {data, error, loading}] = useMutation(DELETE_USER);
+    await deleteUserMutation({
+        variables: {
+            data: {
+                name: user,
+                password: password,
+            }
+        }
+    })
+    if(loading) return 'loading...';
+    if(error){
+        console.log(`[deleteUser function error]: ${error.message}.`);
+        return false;
+    }
+    logOut();
+    return data.deleteUser;
 }
 
-const deleteUserMap = async () => {
-
+const deleteUserMap = async (mapName) => {
+    const [deleteUserMapMutation, {data, error, loading}] = useMutation(DELETE_USER_MAP);
+    await deleteUserMapMutation({
+        variables: {
+            data: {
+                name: user,
+                password: password,
+                mapName: mapName,
+            }
+        }
+    })
+    if(loading) return 'loading...';
+    if(error){
+        console.log(`[deleteUserMap function error]: ${error.message}.`);
+        return false;
+    }
+    return data.deleteUserMap;
 }
 
 export const usehook = () => {
@@ -136,5 +274,7 @@ export const usehook = () => {
         editMyMap: editMyMap,
         deleteUser: deleteUser,
         deleteUserMap: deleteUserMap,
+        user: user,
+        isLogIn: isLogIn,
     })
 }
