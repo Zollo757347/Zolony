ceba614ea1ba86d63a3da21da5b3ad50b23ecd65
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2023-01-05 10:36:05 +0800

    refactor: sign up/sign in/edit profile

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -3,6 +3,7 @@ import { Form, Input, Avatar, Image, Button, Select, Modal, Slider } from 'antd'
 import { RightOutlined } from '@ant-design/icons';
 import Canvas from './Canvas';
 import './css/Info.css'
+import { UseHook } from '../hook/usehook';
 
 const { TextArea } = Input;
 const Info = ({ setOpenModal }) => {
@@ -12,121 +13,119 @@ const Info = ({ setOpenModal }) => {
     const [ylen, setYlen] = useState(5);
     const [zlen, setZlen] = useState(5);
 
-    const check = () => setSelectItems([
-        {
-            value: 'jack',
-            label: 'Jack',
-        },
-        {
-            value: 'lucy',
-            label: 'Lucy',
-        },
-        {
-            value: 'tom',
-            label: 'Tom',
-        },
-    ])
+    const { user, bio } = UseHook();
 
-    const onSelect = (value) => {
-        console.log(value)
-    }
+  const check = () => setSelectItems([
+    {
+      value: 'jack',
+      label: 'Jack',
+    },
+    {
+      value: 'lucy',
+      label: 'Lucy',
+    },
+    {
+      value: 'tom',
+      label: 'Tom',
+    },
+  ])
 
-    const profileForm = <>
-        <Form>
-            <div id='Info-username' className='Info-left'>
-                <Form.Item label="Username">
-                    <Input defaultValue="Username" disabled={true}/>
-                </Form.Item>
-            </div>
-            <div id='Info-avator' className='Info-left'>
-                <Avatar
-                    src={<Image src={require("./data/img/header/wordmark.png")}/>}
-                    style={{
-                        width: 400,
-                        height: 400,
-                    }}
-                />
-            </div>
-            <div id='Info-bio' className='Info-left'>
-                <Form.Item label="Bio">
-                    <TextArea rows={4} defaultValue="在這裡放上自介" disabled={true}/>
-                </Form.Item>
-            </div>
-            <div id='Info-btn' className='Info-left'>
-                <Form.Item>
-                    <Button onClick={() => setOpenModal(3)}>
-                        修改密碼/編輯個人資料
-                    </Button>
-                </Form.Item>
-            </div>
-        </Form>
-    </>
+  const onSelect = (value) => {
+    console.log(value)
+  }
 
-    const mapForm = <>
-        <div id='Info-select'>
+  const profileForm = <>
+    <Form>
+      <div id='Info-username' className='Info-left'>
+        <Form.Item label="Username">
+          <Input defaultValue={user} disabled={true}/>
+        </Form.Item>
+      </div>
+      <div id='Info-avator' className='Info-left'>
+        <Avatar
+          src={<Image src={require("./data/img/header/wordmark.png")}/>}
+          style={{
+            width: 400,
+            height: 400,
+          }}
+        />
+      </div>
+      <div id='Info-bio' className='Info-left'>
+        <Form.Item label="Bio">
+          <TextArea rows={4} defaultValue={bio ?? "在這裡放上自介"} disabled={true}/>
+        </Form.Item>
+      </div>
+      <div id='Info-btn' className='Info-left'>
+        <Form.Item>
+          <Button onClick={() => setOpenModal(3)}>
+            修改密碼/編輯個人資料
+          </Button>
+        </Form.Item>
+      </div>
+    </Form>
+  </>
+
+  const MapModal = <>
+    <Input placeholder="輸入你的地圖名稱" prefix={<RightOutlined />} onChange={e => console.log(e.target.value)}/>
+    <br/>
+    <br/>
+    <span> 輸入長寬高↓ </span>
+    <div id='Map-Modal-xyz-wrapper'>
+      <Input placeholder="xlen" className='Map-Modal-xyz' onChange={e => console.log(e.target.value)}/>
+      <Input placeholder="ylen" className='Map-Modal-xyz' onChange={e => console.log(e.target.value)}/>
+      <Input placeholder="zlen" className='Map-Modal-xyz' onChange={e => console.log(e.target.value)}/>
+    </div>
+  </>
+
+  return(
+    <div id='Info-wrapper'>
+      <div id='Info-left-wrapper'>
+        {profileForm}
+      </div>
+      <div id='Info-right-wrapper'>
+        <div id='Info-right-header'>
+          <div id='Info-select'>
             <Form><Form.Item><Select
-                showSearch
-                placeholder="Select a map"
-                optionFilterProp="children"
-                onSelect={onSelect}
-                style = {{
-                    width: '100%'
-                }}
-                filterOption={(input, option) =>
-                    (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
-                }
-                options={selectItems}
+              showSearch
+              placeholder="Select a map"
+              optionFilterProp="children"
+              onSelect={onSelect}
+              style = {{
+                width: '100%'
+              }}
+              filterOption={(input, option) =>
+                (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+              }
+              options={selectItems}
             /></Form.Item></Form>
-        </div>
-        <div id='Info-del'>
+          </div>
+          <div id='Info-del'>
             <Button onClick={check} className="Info-del-btn">
-                刪減目前地圖
+              刪減目前地圖
             </Button>
-        </div>
-        <div id='Info-add'>
+          </div>
+          <div id='Info-add'>
             <Button onClick={() => setOpenMapModal(true)} className="Info-add-btn"> 
-                增加地圖
+              增加地圖
             </Button>
+          </div>
         </div>
-    </>
-
-    const mapModalContent = <>
-        <Input placeholder="輸入你的地圖名稱" prefix={<RightOutlined />} onChange={e => console.log(e.target.value)}/>
-        <br/>
-        <br/>
-        <span> 長(x): {xlen} , 寬(y): {ylen} , 高(z): {zlen} </span>
-        <div id='Map-modal-xyz-wrapper'>
-            <Slider max={15} min={1} defaultValue={xlen} onChange={(value) => setXlen(value)}/>
-            <Slider max={15} min={1} defaultValue={ylen} onChange={(value) => setYlen(value)}/>
-            <Slider max={15} min={1} defaultValue={zlen} onChange={(value) => setZlen(value)}/>
+        <div id='Info-right-section'>
+          <Canvas canvaswidth={500} canvasheight={500} xlen={5} ylen={5} zlen={5}/>
         </div>
-    </>
+      </div>
 
-    return(
-        <div id='Info-wrapper'>
-            <div id='Info-left-wrapper'>
-                {profileForm}
-            </div>
-            <div id='Info-right-wrapper'>
-                <div id='Info-right-header'>
-                    {mapForm}
-                </div>
-                <div id='Info-right-section'>
-                    <Canvas canvaswidth={500} canvasheight={500} xlen={5} ylen={5} zlen={5}/>
-                </div>
-            </div>
-
-            <Modal
-                title="新增地圖"
-                centered
-                open={openMapModal}
-                onOk={() => {setOpenMapModal(false);}}
-                onCancel={() => setOpenMapModal(false)}
-            >
-                {mapModalContent}
-            </Modal>
-        </div>
-    );
+      <Modal
+        title="新增地圖"
+        centered
+        open={openMapModal}
+        onOk={() => {setOpenMapModal(false);}}
+        onCancel={() => setOpenMapModal(false)}
+      >
+        {MapModal}
+      </Modal>
+    </div>
+  );
 }
 
 export default Info;
--- frontend/src/hook/usehook.js
+++ frontend/src/hook/usehook.js
@@ -1,292 +1,300 @@
 import { useLazyQuery, useMutation } from "@apollo/client";
 import CryptoJs from 'crypto-js'
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState } from "react";
 import {LOG_IN, GET_MAP, CREATE_ACCOUNT, EDIT_PROFILE, INITIAL_MY_MAP, EDIT_MY_MAP, DELETE_USER, DELETE_USER_MAP} from '../graphql';
 
 
 const HookContext = createContext({
-    LogIn: () => {},
-    LogOut: () => {},
-    GetMap: () => {},
-    CreateAccount: () => {},
-    EditProfile: () => {},
-    InitialMyMap: () => {},
-    EditMyMap: () => {},
-    DeleteUser: () => {},
-    DeleteUserMap: () => {},
-    user: '',
-    isLogIn: false,
-  });
+  logIn: async () => {},
+  LogOut: () => {},
+  GetMap: () => {},
+  createAccount: async () => {},
+  editProfile: async () => {},
+  InitialMyMap: () => {},
+  EditMyMap: () => {},
+  DeleteUser: () => {},
+  DeleteUserMap: () => {},
+  setUser: () => {}, 
+  setBio: () => {}, 
+  setAvatar: () => {},
+  user: '',
+  bio: '',
+  avatar: '',
+  isLogIn: false,
+});
 
 
 const HookProvider = (props) => {
-    const [user, setUser] = useState('');
-    const [password, setPassword] = useState('');
-    const [isLogIn, setIsLogIn] = useState(false);
+  const [user, setUser] = useState('');
+  const [bio, setBio] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [isLogIn, setIsLogIn] = useState(false);
 
-    const [createAccountMutation] = useMutation(CREATE_ACCOUNT);
-    const [editProfileMutation] = useMutation(EDIT_PROFILE);
-    const [initialMyMapMutation] = useMutation(INITIAL_MY_MAP);
-    const [editMyMapMutation] = useMutation(EDIT_MY_MAP);
-    const [deleteUserMutation] = useMutation(DELETE_USER);
-    const [deleteUserMapMutation] = useMutation(DELETE_USER_MAP);
-    const [logInQuery] = useLazyQuery(LOG_IN);
-    const [getMapQuery] = useLazyQuery(GET_MAP);
+  const [createAccountMutation] = useMutation(CREATE_ACCOUNT);
+  const [editProfileMutation] = useMutation(EDIT_PROFILE);
+  const [initialMyMapMutation] = useMutation(INITIAL_MY_MAP);
+  const [editMyMapMutation] = useMutation(EDIT_MY_MAP);
+  const [deleteUserMutation] = useMutation(DELETE_USER);
+  const [deleteUserMapMutation] = useMutation(DELETE_USER_MAP);
+  const [logInQuery] = useLazyQuery(LOG_IN);
+  const [getMapQuery] = useLazyQuery(GET_MAP);
 
-    const LogIn = async (name, pwd) => {
-        const cryptopwd = CryptoJs.MD5(pwd).toString();
-        const {error, loading, data} = await logInQuery({
-            variables: {
-                name: name,
-                password: cryptopwd,
-            }
-        })
-        if(loading) return 'loading...';
-        if(error){
-            console.log(`[logIn function error]: ${error.message}.`);
-            return 'error';
-        }
-        else {
-            if(!data.logIn){
-                console.log(`user not found.`);
-                return 'user not found';
-            } 
-            setUser(name);
-            setPassword(cryptopwd);
-            setIsLogIn(true);
-            console.log(`data query succeed.`);
-            console.log(data.logIn);
-            return data.logIn;
-        }
+  const logIn = async (name, pwd) => {
+    const cryptopwd = CryptoJs.MD5(pwd).toString();
+
+    const {error, loading, data} = await logInQuery({
+      variables: {
+        name: name,
+        password: cryptopwd,
+      }
+    }).catch(console.log);
+
+    if (loading) return 'loading...';
+    if (error) {
+      console.log(`[logIn function error]: ${error.message}.`);
+      return 'error';
+    }
+    else {
+      if (!data.logIn) {
+        console.log(`user not found.`);
+        return 'invalid';
+      }
+      
+      setIsLogIn(true);
+      setAvatar(data.logIn.avatar);
+      setBio(data.logIn.bio);
+      return data.logIn;
+    }
+  }
+
+  const GetMap = async (name, mapName) => {
+    const {loading, data, error} = await getMapQuery({
+      variables: {
+        name: name,
+        mapName: mapName,
+      }
+    });
+
+    if (loading) return 'loading...';
+    if (error) {
+      console.log(`[getMap function error]: ${error.message}.`);
+      return 'error';
     }
 
-    const GetMap = async (name, mapName) => {
-        const {loading, data, error} = await getMapQuery({
-            variables: {
-                name: name,
-                mapName: mapName,
-            }
-        });
-        if(loading) return 'loading...';
-        if(error){
-            console.log(`[getMap function error]: ${error.message}.`);
-            return 'error';
-        }
-        else {
-            if(!data.getMap){
-                console.log(`user not found.`);
-                return 'user not found';
-            }
-            console.log(`getmap succeed.`);
-            console.log(data.getMap);
-            return data.getMap;
-        }
+    if (!data.getMap) {
+      console.log(`user not found.`);
+      return 'user not found';
     }
+    
+    console.log(`getmap succeed.`);
+    console.log(data.getMap);
+    return data.getMap;
+  }
 
-    const CreateAccount = async (name, pwd) => {
-        const cryptopwd = CryptoJs.MD5(pwd).toString();
-        const {loading, data, error} = await createAccountMutation({
-            variables: {
-                name: name,
-                password: cryptopwd,
-            }
-        });
+  const createAccount = async (name, pwd) => {
+    const cryptopwd = CryptoJs.MD5(pwd).toString();
+    const { loading, data, error } = await createAccountMutation({
+      variables: {
+        name: name,
+        password: cryptopwd,
+      }
+    });
 
-        if(loading) return 'loading...';
-        if(error){
-            console.log(`[createAccount function error]: ${error.message}.`);
-            return 'error';
-        }
-        else {
-            if(!data.createAccount){
-                console.log(`${name} already used.`);
-                return '';
-            } 
-            setUser(name);
-            setPassword(cryptopwd);
-            setIsLogIn(true);
-            return data.createAccount;
-        }
+    if (loading) return 'loading...';
+    if (error) {
+      console.log(`[createAccount function error]: ${error.message}.`);
+      return 'error';
     }
-
-    const LogOut = () => {
-        setUser('');
-        setPassword('');
-        setIsLogIn(false);
+    else {
+      if (!data.createAccount) {
+        console.log(`${name} already used.`);
+        return 'exist';
+      }
+      
+      setIsLogIn(true);
+      return data.createAccount;
     }
+  }
 
-    /* must wrap into a object
+  const LogOut = () => {
+    setUser('');
+    setIsLogIn(false);
+  }
 
-    input = {
-        newPassword,
-        newAvatar,
-        newBio,
-        newLevel
+  const editProfile = async (name, pwd, input) => {
+    const cryptopwd = CryptoJs.MD5(pwd).toString();
+
+    const {loading, data, error} = await editProfileMutation({
+      variables: {
+        name: name,
+        password: cryptopwd,
+        newPassword: input.newPassword ? CryptoJs.MD5(input.newPassword).toString() : undefined,
+        newAvatar: input.newAvatar,
+        newBio: input.newBio,
+        newLevel: input.newLevel,
+      }
+    });
+
+    if (loading) return 'loading...';
+    if (error) {
+      return 'error';
     }
-    */
+    else {
+      if (!data.editProfile) {
+        return 'invalid';
+      }
+      if (pwd === input.newPassword) {
+        return 'password';
+      }
 
-    const EditProfile = async (input) => {
-        
-        const {loading, data, error} = await editProfileMutation({
-            variables: {
-                name: user,
-                password: password,
-                newPassword: input.newPassword ? CryptoJs.MD5(input.newPassword).toString() : undefined,
-                newAvatar: input.newAvatar,
-                newBio: input.newBio,
-                newLevel: input.newLevel,
-            }
-        })
-        if(loading) return 'loading...';
-        if(error){
-            console.log(`[editProfile function error]: ${error.message}.`);
-            return 'error';
-        }
-        else {
-            if(!data.editProfile){
-                console.log(`user not found.`);
-                return 'user not found';
-            } 
-            setPassword(CryptoJs.MD5(input.newPassword).toString());
-            console.log(`edit profile succeed.`)
-            return data.editProfile;
-        }
+      setAvatar(data.editProfile.avatar);
+      setBio(data.editProfile.bio);
+      return data.editProfile;
     }
+  }
 
-    const InitialMyMap = async (xLen, yLen, zLen, mapName) => {
-        
-        const {loading, data, error} = await initialMyMapMutation({
-            variables: {
-                name: user,
-                password: password,
-                mapName: mapName,
-                xLen: xLen,
-                yLen: yLen,
-                zLen: zLen,
-            }
-        })
-        if(loading) return 'loading...';
-        if(error){
-            console.log(`[initialMyMap function error]: ${error.message}.`);
-            return 'error';
-        }
-        else {
-            if(!data.initialMyMap){
-                console.log(`map already exist.`);
-                return 'map already exist';
-            } 
-            console.log(`initialmap succeed`);
-            console.log(data.initialMyMap);
-            return data.initialMyMap;
-        }
+  const InitialMyMap = async (user, pwd, xLen, yLen, zLen, mapName) => {
+    const cryptopwd = CryptoJs.MD5(pwd).toString();
+    
+    const {loading, data, error} = await initialMyMapMutation({
+      variables: {
+        name: user,
+        password: cryptopwd,
+        mapName: mapName,
+        xLen: xLen,
+        yLen: yLen,
+        zLen: zLen,
+      }
+    })
+    if(loading) return 'loading...';
+    if(error){
+      console.log(`[initialMyMap function error]: ${error.message}.`);
+      return 'error';
     }
-
-    /* must wrap into a object
-
-    map = {
-        xLen,
-        yLen,
-        zLen,
-        mapName,
-        playground [[[{
-            blockName,
-            type,
-            breakable,
-            states {
-                power,
-                source,
-                delay,
-                facing,
-                face,
-                locked,
-                powered,
-                lit,
-                east,
-                south,
-                west,
-                north,
-            }
-        }]]],
+    else {
+      if(!data.initialMyMap){
+        console.log(`map already exist.`);
+        return 'map already exist';
+      } 
+      console.log(`initialmap succeed`);
+      console.log(data.initialMyMap);
+      return data.initialMyMap;
     }
-    */
+  }
 
-    const EditMyMap = async (map) => {
-        
-        const {loading, data, error} = await editMyMapMutation({
-            variables: {
-                name: user,
-                password: password,
-                mapName: map.mapName,
-                map: map,
-            }
-        })
-        if(loading) return 'loading...';
-        if(error){
-            console.log(`[editMyMap function error]: ${error.message}.`);
-            return 'error';
-        }
-        else {
-            if(!data.editMyMap){
-                console.log(`save map fail.`);
-                return 'map already exist';
-            } 
-            console.log(`edit map succeed.`)
-            console.log(data.editMyMap);
-            return data.editMyMap;
-        }
-    }
+  /* must wrap into a object
 
-    const DeleteUser = async () => {
-        
-        const {loading, data, error} = await deleteUserMutation({
-            variables: {
-                name: user,
-                password: password,
-            }
-        })
-        if(loading) return 'loading...';
-        if(error){
-            console.log(`[deleteUser function error]: ${error.message}.`);
-            return false;
-        }
-        LogOut();
-        return data.deleteUser;
-    }
+  map = {
+    xLen,
+    yLen,
+    zLen,
+    mapName,
+    playground [[[{
+      blockName,
+      type,
+      breakable,
+      states {
+        power,
+        source,
+        delay,
+        facing,
+        face,
+        locked,
+        powered,
+        lit,
+        east,
+        south,
+        west,
+        north,
+      }
+    }]]],
+  }
+  */
 
-    const DeleteUserMap = async (mapName) => {
-        
-        const {loading, data, error} = await deleteUserMapMutation({
-            variables: {
-                name: user,
-                password: password,
-                mapName: mapName,
-            }
-        })
-        if(loading) return 'loading...';
-        if(error){
-            console.log(`[deleteUserMap function error]: ${error.message}.`);
-            return false;
-        }
-        return data.deleteUserMap;
+  const EditMyMap = async (user, pwd, map) => {
+    const cryptopwd = CryptoJs.MD5(pwd).toString();
+    
+    const {loading, data, error} = await editMyMapMutation({
+      variables: {
+        name: user,
+        password: cryptopwd,
+        mapName: map.mapName,
+        map: map,
+      }
+    })
+    if(loading) return 'loading...';
+    if(error){
+      console.log(`[editMyMap function error]: ${error.message}.`);
+      return 'error';
     }
-    return (
-        <HookContext.Provider
-            value = {{
-                LogIn,
-                LogOut,
-                GetMap,
-                CreateAccount,
-                EditProfile,
-                InitialMyMap,
-                EditMyMap,
-                DeleteUser,
-                DeleteUserMap,
-                user,
-                isLogIn,
-            }}
-            {...props}
-        />
-    );  
+    else {
+      if(!data.editMyMap){
+        console.log(`save map fail.`);
+        return 'map already exist';
+      } 
+      console.log(`edit map succeed.`)
+      console.log(data.editMyMap);
+      return data.editMyMap;
+    }
+  }
+
+  const DeleteUser = async (user, pwd) => {
+    const cryptopwd = CryptoJs.MD5(pwd).toString();
+    
+    const {loading, data, error} = await deleteUserMutation({
+      variables: {
+        name: user,
+        password: cryptopwd,
+      }
+    })
+    if(loading) return 'loading...';
+    if(error){
+      console.log(`[deleteUser function error]: ${error.message}.`);
+      return false;
+    }
+    LogOut();
+    return data.deleteUser;
+  }
+
+  const DeleteUserMap = async (user, pwd, mapName) => {
+    
+    const {loading, data, error} = await deleteUserMapMutation({
+      variables: {
+        name: user,
+        password: pwd,
+        mapName: mapName,
+      }
+    })
+    if(loading) return 'loading...';
+    if(error){
+      console.log(`[deleteUserMap function error]: ${error.message}.`);
+      return false;
+    }
+    return data.deleteUserMap;
+  }
+  return (
+    <HookContext.Provider
+      value = {{
+        logIn,
+        LogOut,
+        GetMap,
+        createAccount,
+        editProfile,
+        InitialMyMap,
+        EditMyMap,
+        DeleteUser,
+        DeleteUserMap,
+        setUser,
+        setBio, 
+        setAvatar, 
+        user,
+        bio, 
+        avatar, 
+        isLogIn,
+      }}
+      {...props}
+    />
+  );  
 }
 
 const UseHook = () => useContext(HookContext);
