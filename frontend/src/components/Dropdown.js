54474eccdf20564dd9375f24f2fb71832dba4da5
AhWeiHowXun <96255407+AhWeiHowXun@users.noreply.github.com>
2023-01-05 16:49:02 +0800

    fix dropdown-content

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { DownOutlined } from '@ant-design/icons';
-import { Dropdown, Space } from 'antd';
+import { Avatar, Dropdown, Space } from 'antd';
 import './css/Dropdown.css'
 import { UseHook } from '../hook/usehook';
 
 const Dropdown_Components = ({ setOpenModal }) => {
-    const { isLogIn , LogOut, setPageNum } = UseHook();
+    const { isLogIn , LogOut, avatar, setPageNum } = UseHook();
     
     const onClick = ({ key }) => {
         switch(key) {
@@ -30,23 +30,23 @@ const Dropdown_Components = ({ setOpenModal }) => {
     const items = [
         {
             key: '1',
-            label: <div>Your Profile</div>,
+            label: 'Your Profile',
             disabled: (!isLogIn)
         },
         {
             key: '2',
-            label: <div>Log in</div>,
+            label: 'Log in',
             disabled: (isLogIn)
         },
         {
             key: '3',
-            label: <div>Sign up</div>,
+            label: 'Sign up',
             disabled: (isLogIn)
         },
         {
             key: '4',
             danger: true,
-            label: <div onClick={() => {LogOut(); setPageNum(1);}}>Log out</div>,
+            label: 'Log out',
             disabled: (!isLogIn)
         }
     ];
@@ -62,7 +62,7 @@ const Dropdown_Components = ({ setOpenModal }) => {
         >
             <span onClick={(e) => e.preventDefault()}>
                 <Space>
-                    <b>Account</b>
+                    {isLogIn ? <Avatar src={avatar}/> :  <b>Account</b>}
                     <DownOutlined />
                 </Space>
             </span>
