a9495c6eb57c9cf66331bf1919a1bdcd1d4d8604
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2023-01-27 19:10:30 +0800

    refactor: use styled-component in Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,9 @@
-import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import { useState } from 'react';
 import styled from 'styled-components';
 import Button from './Button';
 import Dropdown from './Dropdown';
 import Sidebar from './Sidebar';
-import './css/Header.css'
 import { useHook } from '../hooks/useHook';
-
-const HeaderWrapper = styled.div`
-  background-color: rgb(245, 213, 37);
-  height: 80px;
-  position: relative;
-  z-index: 2011;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const SidebarWrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: row;
-`;
 
 const Header = ({ setOpenModal }) => {
   const [collapsed, setCollapsed] = useState(true);
@@ -30,27 +12,55 @@ const Header = ({ setOpenModal }) => {
   return (
     <>
       <HeaderWrapper>
-        <div id='header-left'>
-          <Button
-            onClick={() => setCollapsed(!collapsed)}
-            id="sidebar-button"
-          >
-            {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        <LeftHeaderWrapper>
+          <Button onClick={() => setCollapsed(!collapsed)} id="sidebar-button">
+            {collapsed ? '^' : 'v'}
           </Button>
-          <div id="wordmark" onClick={() => setPageNum(1)}><img src={require("./data/img/header/wordmark.png")} alt="Wordmark"/></div>
-        </div>
-        <div id='header-right'>
-          <Dropdown
-            setPageToInfo={() => setPageNum(0)}
-            setOpenModal={setOpenModal}
-          />
-        </div>
+          <StyledWordmark src={require("./data/img/header/wordmark.png")} alt="Wordmark" onClick={() => setPageNum(1)} />
+        </LeftHeaderWrapper>
+        <RightHeaderWrapper>
+          <Dropdown setPageToInfo={() => setPageNum(0)} setOpenModal={setOpenModal} />
+        </RightHeaderWrapper>
       </HeaderWrapper>
-      <SidebarWrapper>
-        <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
-      </SidebarWrapper>
+      <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
     </>
   );
 }
+
+const HeaderWrapper = styled.div`
+  background-color: rgb(245, 213, 37);
+  height: 80px;
+  position: sticky;
+  top: 0;
+  z-index: 2011;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LeftHeaderWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+`;
+
+const RightHeaderWrapper = styled.div`
+  margin-right: 20px;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledWordmark = styled.img`
+  height: 70px;
+  margin-left: 15px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export default Header;
